76ade15b5f55275cc951847a5510ed995a919ca4
Miles Eng <miles.eng@duke.edu>
2023-10-26 15:11:34 -0400

    added spotify login button and auth code

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -8,15 +8,31 @@ const codeChallenge = base64encode(codeVerifier)
 
 
 const HomeScreen = () => {
-  const [accessToken, setAccessToken] = useState()
 
-  const loginURL = {
+  const CLIENT_ID = import.meta.env.VITE_CLIENT_ID
+  const REDIRECT_URI = import.meta.env.VITE_DEV_REDIRECT_URI
+  const auth_url = new URL(import.meta.env.VITE_AUTH_ENDPOINT)
+  const scope = 'streaming user-read-email user-read-private user-top-read'
 
+  const loginParams = {
+    response_type: 'code',
+    client_id: CLIENT_ID,
+    redirect_uri: REDIRECT_URI,
+    code_challenge_method: 'S256',
+    code_challenge: codeChallenge,
+    scope: scope
   }
+
+  auth_url.search = new URLSearchParams(loginParams).toString()
+
+  useEffect(() => {
+    window.localStorage.removeItem('access_token')
+    window.localStorage.removeItem('refresh_token')
+  }, [])
 
   return (
     <div>
-      <a href={loginURL}>Log in</a>
+      <a href={auth_url}>Log in</a>
     </div>
   )
 }
